f895c315a9e14d71f2cce6babecccd9a51914278
Mark <wernsdorfer@gmail.com>
2019-03-25 10:47:01 +0100

    foinally!

--- src/map_generation.py
+++ src/map_generation.py
@@ -11,7 +11,7 @@ def my_range(no_samples: int, start: float = 0., end: float = 1., start_point: b
     start_value = start + float_not_start * step_size
 
     for _ in range(no_samples):
-        yield start_value
+        yield min(end, max(start, start_value))
         start_value += step_size
 
     return
@@ -57,20 +57,41 @@ class Map:
         if .5 < mean:
             if no_samples_right == 1:
                 samples_right = [1.]
+
             else:
                 samples_right = list(my_range(no_samples_right, start_point=True, end_point=True, start=mean, end=1.))
-            deviance = sum(_s - mean for _s in samples_right)
-            step = deviance / (no_samples_left ** 2.)
-            samples_left = [mean - (_i + 1.) * step for _i in range(no_samples_left)][::-1]
+
+            mean_left = mean - sum(_s - mean for _s in samples_right) / no_samples_left
+            if no_samples_left == 1:
+                samples_left = [max(0., mean_left)]
+
+            else:
+                radius_left = min(mean_left, mean - mean_left)
+                samples_left = list(my_range(
+                    no_samples_left,
+                    start_point=True, end_point=True,
+                    start=max(0., mean_left-radius_left),
+                    end=min(mean_left+radius_left, mean),
+                ))
 
         else:
             if no_samples_left == 1:
                 samples_left = [0.]
             else:
                 samples_left = list(my_range(no_samples_left, start_point=True, end_point=True, start=0, end=mean))
-            deviance = sum(mean - _s for _s in samples_left)
-            step = deviance / (no_samples_right ** 2.)
-            samples_right = [mean + (_i + 1) * step for _i in range(no_samples_right)]
+
+            mean_right = mean + sum(mean - _s for _s in samples_left) / no_samples_right
+            if no_samples_right == 1:
+                samples_right = [min(1., mean_right)]
+
+            else:
+                radius_right = min(mean_right - mean, 1. - mean_right)
+                samples_right = list(my_range(
+                    no_samples_right,
+                    start_point=True, end_point=True,
+                    start=max(mean, mean_right-radius_right),
+                    end=min(1., mean_right+radius_right),
+                ))
 
         return samples_left + samples_right
 
@@ -102,16 +123,16 @@ def sample_set_to_string(samples: Iterable[Sequence[float]], precision: int = 3,
 def main():
     random.seed(345363)
 
-    # samples = Map._multi_sample_uniform((.5, 1., .25), 4)
     for _i in range(1000):
-        no_samples = random.randint(1, 6)
+        no_samples = random.randint(1, 12)
         target_average = random.random()
         samples = Map._single_sample_uniform(no_samples, target_average)
         average = sum(samples) / len(samples)
-        if abs(average - target_average) >= .02:
+        if abs(average - target_average) >= .02 or not all(1. >= _x >= 0. for _x in samples):
             print(f"{_i:04}:\n"
                   f"no_samples: {no_samples:02d}, target_average: {target_average:.03f}, actual_average: {average:0.3f}")
             print(sample_to_string(samples))
+            break
 
 
 if __name__ == "__main__":
