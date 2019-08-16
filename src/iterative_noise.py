477e9c92f8c3ab0ac15cfac16aa4d7d6f098188b
Mark <wernsdorfer@gmail.com>
2019-08-16 16:53:58 +0200

    working 2d random walk

--- src/iterative_noise.py
+++ src/iterative_noise.py
@@ -2,47 +2,57 @@ import random
 
 import numpy
 from PIL import Image
+from matplotlib import pyplot
 
 
-def new_value(old_value: int, step_size: int = 1) -> int:
-    lighter = random.randint(0, 255) < old_value
-    # return max(0, min(255, old_value + step_size * int(lighter) * 2 - 1))
-    return random.randint(old_value, 255) if lighter else random.randint(0, old_value)
+def randomize_value(old_value: int, return_band: int = 50, step_size: int = 1) -> int:
+    return max(0, min(255, old_value + random.choice([-step_size, step_size])))
+
+    if old_value < return_band:
+        return old_value + step_size * int(old_value < random.randint(0, return_band)) * 2 - 1
+
+    if return_band < old_value:
+        return old_value + step_size * int(random.randint(0, return_band) < old_value) * 2 - 1
 
 
 def main():
-    width = 128
+    pyplot.ion()
+
+    width = 32
     height = width
 
-    im = Image.new("L", (width, height))
+    im = Image.new("L", (width, height), color=0)
 
     im.putpixel((0, 0), 128)
 
-    for _lr in range(1, height):
-        top = im.getpixel((0, _lr - 1))
-        value = new_value(top, step_size=10)
-        im.putpixel((0, _lr), value)
+    for _x in range(1, width):
+        left = im.getpixel((_x - 1, 0))
+        value = randomize_value(left, step_size=30)
+        im.putpixel((_x, 0), value)
 
-    for _tb in range(1, width):
-        left = im.getpixel((_tb - 1, 0))
-        value = new_value(left, step_size=10)
-        im.putpixel((_tb, 0), value)
+    for _y in range(1, height):
+        top = im.getpixel((0, _y - 1))
+        value = randomize_value(top, step_size=30)
+        im.putpixel((0, _y), value)
 
-    """
     for _y in range(1, height):
         for _x in range(1, width):
-            top_value = im.getpixel((_y - 1, _x))
-            left_value = im.getpixel((_y, _x - 1))
-            avrg = (top_value + left_value) // 2
-            noise = (int(random.randint(0, 255) < avrg) * 2 - 1)
-            value = avrg + noise
-            im.putpixel((_x, _y), max(0, min(255, value)))
+            top_value = im.getpixel((_x, _y - 1))
+            left_value = im.getpixel((_x - 1, _y))
+            top_left_value = im.getpixel((_x - 1, _y - 1))
+            avrg = (top_value + left_value + top_left_value) // 3
+            value = randomize_value(avrg, step_size=30)
+            im.putpixel((_x, _y), min(255, value))
 
-    """
+            pyplot.pause(.00000001)
+            pyplot.clf()
+            pyplot.imshow(im)
+            pyplot.draw()
+
+    pyplot.show()
     # d = numpy.array(im)
     # print(d)
 
-    im.show()
 
 
 if __name__ == "__main__":
