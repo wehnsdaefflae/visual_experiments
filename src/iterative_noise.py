7d8c86af17beb0c3a4fadc8e389505cd01fc989d
Mark <wernsdorfer@gmail.com>
2019-08-16 17:31:05 +0200

    started nondirectional (filtered) noise

--- src/iterative_noise.py
+++ src/iterative_noise.py
@@ -1,4 +1,5 @@
 import random
+from typing import Sequence
 
 import numpy
 from PIL import Image
@@ -15,13 +16,44 @@ def randomize_value(old_value: int, return_band: int = 50, step_size: int = 1) -
         return old_value + step_size * int(random.randint(0, return_band) < old_value) * 2 - 1
 
 
-def main():
-    pyplot.ion()
+def get_neighbors(image: Image, x: int, y: int) -> Sequence[int]:
+    width, height = image.size
 
-    width = 32
-    height = width
+    if x == 0 and y == 0:
+        return [image.getpixel((x + 1, y)), image.getpixel((x, y + 1))]
 
-    im = Image.new("L", (width, height), color=0)
+    if x == 0 and y == height - 1:
+        return [image.getpixel((x, y - 1)), image.getpixel((x + 1, y))]
+
+    if x == width - 1 and y == 0:
+        return [image.getpixel((x, y + 1)), image.getpixel((x - 1, y))]
+
+    if x == width - 1 and y == height - 1:
+        return [image.getpixel((x, y - 1)), image.getpixel((x - 1, y))]
+
+    return [image.getpixel((x, y - 1)), image.getpixel((x + 1, y)), image.getpixel((x, y + 1)), image.getpixel((x - 1, y))]
+
+
+def nondirectional_noise(im: Image, no_iterations: int = 10):
+    width, height = im.size
+
+    for _x in range(width):
+        for _y in range(height):
+            im.putpixel((_x, _y), random.choice([0, 255]))
+
+    filtered = Image.new("L", (width, height))
+    for _i in range(no_iterations):
+        for _x in range(width):
+            for _y in range(height):
+                neighbors = get_neighbors(im, _x, _y)
+                value = sum(neighbors) // len(neighbors)
+                filtered.putpixel((_x, _y), randomize_value(value, step_size=30))
+
+        im = filtered
+
+
+def directional_noise(im: Image):
+    width, height = im.size
 
     im.putpixel((0, 0), 128)
 
@@ -35,24 +67,35 @@ def main():
         value = randomize_value(top, step_size=30)
         im.putpixel((0, _y), value)
 
+    pyplot.ion()
+
     for _y in range(1, height):
         for _x in range(1, width):
             top_value = im.getpixel((_x, _y - 1))
             left_value = im.getpixel((_x - 1, _y))
             top_left_value = im.getpixel((_x - 1, _y - 1))
-            avrg = (top_value + left_value + top_left_value) // 3
+            avrg = (top_value + left_value) // 2
             value = randomize_value(avrg, step_size=30)
             im.putpixel((_x, _y), min(255, value))
 
-            pyplot.pause(.00000001)
-            pyplot.clf()
-            pyplot.imshow(im)
-            pyplot.draw()
+            #pyplot.pause(.00000001)
+            #pyplot.clf()
+            #pyplot.imshow(im)
+            #pyplot.draw()
 
+    pyplot.ioff()
+
+
+def main():
+    width = 512
+    height = width
+
+    im = Image.new("L", (width, height))
+    # directional_noise(im)
+    nondirectional_noise(im)
+
+    pyplot.imshow(im)
     pyplot.show()
-    # d = numpy.array(im)
-    # print(d)
-
 
 
 if __name__ == "__main__":
