74dba59e264922b2e6d5596e3c56b12a3fb61be8
Mark <wernsdorfer@gmail.com>
2019-08-18 14:42:42 +0200

    new iterative noise for resolutions to the power of 2

--- src/iterative_noise.py
+++ src/iterative_noise.py
@@ -1,9 +1,11 @@
 import random
-from typing import Sequence
+from typing import Sequence, Tuple
 
 import numpy
 from PIL import Image
 from matplotlib import pyplot
+
+from src.brownian_bridge import noisify
 
 
 def randomize_value(old_value: int, return_band: int = 50, step_size: int = 1) -> int:
@@ -35,6 +37,10 @@ def get_neighbors(image: Image, x: int, y: int) -> Sequence[int]:
 def nondirectional_noise(im: Image, no_iterations: int = 10):
     width, height = im.size
 
+    # comb through:
+    # xxx
+    #  x
+
     for _x in range(width):
         for _y in range(height):
             im.putpixel((_x, _y), random.choice([0, 255]))
@@ -52,6 +58,69 @@ def nondirectional_noise(im: Image, no_iterations: int = 10):
         #im.da
         #im = filtered
 
+
+def two_to_the_power_of_what(n: int) -> int:
+    assert is_power_two(n)
+    assert 0 < n
+    _i = 0
+    _n = n
+    while not _n == 1:
+        _n = _n // 2
+        _i += 1
+    return _i
+
+
+def is_power_two(n: int) -> bool:
+    return n and (not(n & (n - 1)))
+
+
+def split_up(value: int, lower_bound: int = 0, upper_bound: int = 255) -> Tuple[int, int, int, int]:
+    return tuple(
+        int(_x)
+        for _x in noisify(
+            [value * 4.], lower_bound=0., upper_bound=255.
+        )
+    )  # type: Tuple[int, int, int, int]
+
+
+def iterative_noise(im: Image, start_value: int = 128):
+    width, height = im.size
+    assert width == height
+    assert is_power_two(width)
+
+    grid = [[start_value]]
+
+    # 1, 2, 4, 8, 16, 32, 64, 128, 256, 512
+    for _i in range(1, two_to_the_power_of_what(width)):
+        p = (_i + 1) ** 2
+
+        new_grid = [
+            [
+                0 for _ in range(p)
+            ] for _ in range(p)
+        ]
+
+        for _y, _row in enumerate(grid):
+            _n_y = _y ** 2
+            new_row = new_grid[_n_y]
+            for _x, _cell in enumerate(_row):
+                new_values = split_up(_cell)
+                _n_x = _x ** 2
+                new_row[_n_x] = new_values[0]
+                if _x < p - 1:
+                    new_row[_n_x + 1] = new_values[1]
+                if _y < p - 1:
+                    new_grid[_n_y + 1][_n_x] = new_values[2]
+                if _x < p - 1 and _y < p - 1:
+                    new_grid[_n_y + 1][_n_x + 1] = new_values[3]
+
+        grid = new_grid
+
+    for _x in range(width):
+        for _y in range(height):
+            im.putpixel((_x, _y), grid[_y][_x])
+
+    return im
 
 def directional_noise(im: Image):
     width, height = im.size
@@ -94,6 +163,7 @@ def main():
     im = Image.new("L", (width, height))
     directional_noise(im)
     # nondirectional_noise(im)
+    iterative_noise(im)
 
     pyplot.imshow(im)
     pyplot.show()
