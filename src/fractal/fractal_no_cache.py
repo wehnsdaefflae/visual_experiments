8adec21dde1a81ffd1aba3a612bb8cfc1c10ed7d
Mark <wernsdorfer@gmail.com>
2019-10-04 19:53:14 +0200

    finished layer prototype, embedding doesn't work

--- src/fractal/fractal_no_cache.py
+++ src/fractal/fractal_no_cache.py
@@ -9,14 +9,14 @@ from PIL import Image
 from PIL import ImageFilter
 from matplotlib import pyplot
 
-GRIDSIZE_RANDOMIZATION_XOFFSET_YOFFSET_FACTOR = Tuple[int, int, int, float]
+GRIDSIZE_RANDOMIZATION_XOFFSET_YOFFSET_FACTOR = Tuple[int, float, int, int, float]
 
 
 def _randomize(value: float, r: float) -> float:
-    return min(1., max(0, value + random.uniform(-r, r)))
+    return min(1., max(0., value + random.uniform(-r, r)))
 
 
-def _set_intermediates(grid: Sequence[List[float]], randomization: float, x_origin: int, y_origin: int, window: int):
+def _set_intermediates(grid: Sequence[List[float]], grid_noise: Sequence[List[float]], randomization: float, x_origin: int, y_origin: int, window: int):
     x_mid = x_origin + window // 2
     y_mid = y_origin + window // 2
 
@@ -24,35 +24,52 @@ def _set_intermediates(grid: Sequence[List[float]], randomization: float, x_orig
     row_mid = grid[y_mid]
     row_bot = grid[y_origin + window]
 
+    noise_row_top = grid_noise[y_origin]
+    noise_row_mid = grid_noise[y_mid]
+    noise_row_bot = grid_noise[y_origin + window]
+
     value_nw = row_top[x_origin]
+    if value_nw < 0.:
+        value_nw = noise_row_top[x_origin]
+
     value_ne = row_top[x_origin + window]
+    if value_ne < 0.:
+        value_ne = noise_row_top[x_origin + window]
+
     value_se = row_bot[x_origin + window]
+    if value_se < 0.:
+        value_se = noise_row_bot[x_origin + window]
+
     value_sw = row_bot[x_origin]
+    if value_sw < 0.:
+        value_sw = noise_row_bot[x_origin]
 
     if row_mid[x_origin + window] < 0.:
-        value_e = (value_ne + value_se) // 2
-        row_mid[x_origin + window] = _randomize(value_e, randomization)
+        value_e = (value_ne + value_se) / 2.
+        noise_row_mid[x_origin + window] = _randomize(value_e, randomization)
 
     if row_bot[x_mid] < 0.:
-        value_s = (value_se + value_sw) // 2
-        row_bot[x_mid] = _randomize(value_s, randomization)
+        value_s = (value_se + value_sw) / 2.
+        noise_row_bot[x_mid] = _randomize(value_s, randomization)
 
     if row_mid[x_mid] < 0.:
-        value_m = (value_nw + value_ne + value_se + value_sw) // 4
-        row_mid[x_mid] = _randomize(value_m, randomization)
+        value_m = (value_nw + value_ne + value_se + value_sw) / 4.
+        noise_row_mid[x_mid] = _randomize(value_m, randomization)
 
     if row_top[x_mid] < 0. and y_origin == 0:
-        value_n = (value_nw + value_ne) // 2
-        row_top[x_mid] = _randomize(value_n, randomization)
+        value_n = (value_nw + value_ne) / 2.
+        noise_row_top[x_mid] = _randomize(value_n, randomization)
 
     if row_mid[x_origin] < 0. and x_origin == 0:
-        value_w = (value_sw + value_nw) // 2
-        row_mid[x_origin] = _randomize(value_w, randomization)
+        value_w = (value_sw + value_nw) / 2.
+        noise_row_mid[x_origin] = _randomize(value_w, randomization)
 
 
-def _get_noise_layer(grid: Sequence[List[float]], grid_noise: Sequence[List[float]], grid_size: int, randomization: int, x_offset: int, y_offset: int) -> Sequence[Sequence[float]]:
+def _get_noise_layer(grid: Sequence[List[float]], grid_size: int, randomization: float, x_offset: int, y_offset: int) -> Sequence[Sequence[float]]:
     size = len(grid)
     window_size = grid_size
+
+    grid_noise = [[-float(value >= 0.) for value in row] for row in grid]
 
     while 1 < window_size:
 
@@ -65,19 +82,19 @@ def _get_noise_layer(grid: Sequence[List[float]], grid_noise: Sequence[List[floa
             while _x < size - window_size:
 
                 if window_size == grid_size:
-                    if -1. < row_noise[_x]:
-                        row_noise[_x] += random.random()
+                    if row_noise[_x] >= 0.:
+                        row_noise[_x] = random.random()
 
-                    if -1. < row_noise[_x + window_size]:
-                        row_noise[_x + window_size] += random.random()
+                    if row_noise[_x + window_size] >= 0.:
+                        row_noise[_x + window_size] = random.random()
 
-                    if -1. < row_next_noise[_x + window_size]:
-                        row_next_noise[_x + window_size] += random.random()
+                    if row_next_noise[_x + window_size] >= 0.:
+                        row_next_noise[_x + window_size] = random.random()
 
-                    if -1 < row_next_noise[_x]:
-                        row_next_noise[_x] += random.random()
+                    if row_next_noise[_x] >= 0.:
+                        row_next_noise[_x] = random.random()
 
-                _set_intermediates(grid, randomization, _x, _y, window_size)
+                _set_intermediates(grid, grid_noise, randomization, _x, _y, window_size)
 
                 _x += window_size
 
@@ -85,21 +102,26 @@ def _get_noise_layer(grid: Sequence[List[float]], grid_noise: Sequence[List[floa
 
         window_size //= 2
 
-    return grid
+    return grid_noise
 
 
-def _create_noise(grid: Sequence[List[float]], components: Sequence[GRIDSIZE_RANDOMIZATION_XOFFSET_YOFFSET_FACTOR]) -> Sequence[Sequence[float]]:
-    no_components = len(components)
+def _create_noise(grid: Sequence[List[float]], components: Sequence[GRIDSIZE_RANDOMIZATION_XOFFSET_YOFFSET_FACTOR]):
+    noise_sum = [[0. for _ in row] for row in grid]
+    factor_sum = 0.
 
     for grid_size, randomization, x_offset, y_offset, factor in components:
-        noise_grid = [[-1. if value != 0. else 0. for value in row] for row in grid]
+        noise_layer = _get_noise_layer(grid, grid_size, randomization, x_offset, y_offset)
 
-        for row, row_layer in zip(grid, _get_noise_layer(grid, noise_grid, grid_size, randomization, x_offset, y_offset)):
+        for row, row_layer in zip(noise_sum, noise_layer):
             for _x, value in enumerate(row_layer):
-                s = row[_x] + value * factor / no_components
+                s = row[_x] + value * factor
                 row[_x] = s
+        factor_sum += factor
 
-    return grid
+    for row, noise_row in zip(grid, noise_sum):
+        for _x, value in enumerate(noise_row):
+            if row[_x] < 0.:
+                row[_x] = value / factor_sum
 
 
 def _render(image: Image, skip: bool = False) -> Image:
@@ -397,6 +419,32 @@ class Map:
 
 
 def main():
+    size = 512
+    # todo: set some pixels
+    grid = [[.5 if 240 < _y < 272 or 240 < _x < 272 else -1. for _x in range(size + 1)] for _y in range(size + 1)]
+    components = (
+        (size // 32, size / (32. * 256.), 0, 0, 1.0),
+        (size // 16, size / (16. * 256.), 0, 0, 1.0),
+        (size // 8, size / (8. * 256.), 0, 0, 1.0),
+        (size // 4, size / (4. * 256.), 0, 0, 1.0),
+        (size // 2, size / (2. * 256.), 0, 0, 1.0),
+    )
+    _create_noise(grid, components)
+
+    for row in grid:
+        for _x in range(len(row)):
+            row[_x] *= 255
+
+    image = Image.fromarray(numpy.uint8(grid), "L")
+    image = _render(image, skip=True)
+
+    fig, ax = pyplot.subplots()
+
+    pyplot.imshow(image, cmap="gist_earth", vmin=0., vmax=256.)
+    fig.canvas.draw()
+    pyplot.show()
+
+def _main():
     size = 256
     map_tiles = Map(tile_size=size + 1, grid_size=size // 8, offset=size // 8, randomization=size // 8)
 
@@ -435,5 +483,5 @@ def main():
 
 
 if __name__ == "__main__":
-    random.seed(2346464)
+    # random.seed(2346464)
     main()
