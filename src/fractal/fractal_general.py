fe0d2a10ed8fb3543c432a7e20c374843c657321
Mark <wernsdorfer@gmail.com>
2019-10-07 09:07:54 +0200

    transparency embedding

--- src/fractal/fractal_general.py
+++ src/fractal/fractal_general.py
@@ -120,6 +120,10 @@ def _add_noise(grid: Sequence[List[float]], tile_size: int, randomization: float
         window_size //= 2
 
 
+# TODO:
+#   0.: implement complete 1-dimensional case
+#   1.: make n-dimensional
+#   2.: add offset in each dimension (not as parameters but integrated)
 def _create_noise(grid: Sequence[List[float]], components: Sequence[TILESIZE_RANDOMIZATION_FACTOR]) -> Sequence[List[float]]:
     assert is_power_two(len(grid) - 1)
     assert all(is_power_two(len(row) - 1) for row in grid)
@@ -145,11 +149,12 @@ def _create_noise(grid: Sequence[List[float]], components: Sequence[TILESIZE_RAN
 
 
 class Map:
-    def __init__(self, grid_size: int = 512 + 1, tile_size: int = 256, offset: int = 64, value_min: int = 0, value_max: int = 255, grid_initial: Optional[Sequence[Sequence[int]]] = None):
+    def __init__(self, grid_size: int = 512 + 1, tile_size: int = 256, distance_transition: int = 64, value_min: int = 0, value_max: int = 255, grid_initial: Optional[Sequence[Sequence[int]]] = None):
         self._grid_size = grid_size
-        self._offset = offset
+        self._distance_transition = distance_transition
         self._value_min, self._value_max = value_min, value_max
         self._components = (
+            # tile size, randomization, factor
             (tile_size, self._grid_size / (4. * 2048.), 1.0),
             # (tile_size // 4, self._grid_size / 2048., .25),
         )
@@ -170,14 +175,14 @@ class Map:
 
     def move_north(self):
         for x in range(self._grid_size):
-            for y in range(self._grid_size - self._offset - 1, -1, -1):
+            for y in range(self._grid_size - self._distance_transition - 1, -1, -1):
                 row = self._tile_current[y]
                 value = row[x]
 
-                row_next = self._tile_current[y + self._offset]
+                row_next = self._tile_current[y + self._distance_transition]
                 row_next[x] = value
 
-            for y in range(self._offset):
+            for y in range(self._distance_transition):
                 row = self._tile_current[y]
                 row[x] = -1.
 
@@ -186,24 +191,24 @@ class Map:
     def move_east(self):
         for y in range(self._grid_size):
             row = self._tile_current[y]
-            for x in range(self._offset, self._grid_size):
+            for x in range(self._distance_transition, self._grid_size):
                 value = row[x]
-                row[x - self._offset] = value
+                row[x - self._distance_transition] = value
 
-            for x in range(self._grid_size - self._offset, self._grid_size):
+            for x in range(self._grid_size - self._distance_transition, self._grid_size):
                 row[x] = -1.
 
         self._tile_current = _create_noise(self._tile_current, self._components)
 
     def move_south(self):
         for x in range(self._grid_size):
-            for y in range(self._offset, self._grid_size):
+            for y in range(self._distance_transition, self._grid_size):
                 row = self._tile_current[y]
-                row_prev = self._tile_current[y - self._offset]
+                row_prev = self._tile_current[y - self._distance_transition]
                 value = row[x]
                 row_prev[x] = value
 
-            for y in range(self._grid_size - self._offset, self._grid_size):
+            for y in range(self._grid_size - self._distance_transition, self._grid_size):
                 row = self._tile_current[y]
                 row[x] = -1.
 
@@ -212,11 +217,11 @@ class Map:
     def move_west(self):
         for y in range(self._grid_size):
             row = self._tile_current[y]
-            for x in range(self._grid_size - self._offset - 1, -1, -1):
+            for x in range(self._grid_size - self._distance_transition - 1, -1, -1):
                 value = row[x]
-                row[x + self._offset] = value
+                row[x + self._distance_transition] = value
 
-            for x in range(self._offset):
+            for x in range(self._distance_transition):
                 row[x] = -1.
 
         self._tile_current = _create_noise(self._tile_current, self._components)
@@ -368,7 +373,7 @@ def main():
     grid_color = load_picture(pic_02, channel=0, x_offset=200, y_offset=200, size=size+1)
     grid_opaque = load_picture(pic_02, channel=3, x_offset=200, y_offset=200, size=size+1)
     grid_color = [[-1. if row_transparency[_x] == 0. else _v for _x, _v in enumerate(row_color)] for row_transparency, row_color in zip(grid_opaque, grid_color)]
-    map_tiles = Map(grid_size=size + 1, tile_size=128, offset=128, grid_initial=grid_color)
+    map_tiles = Map(grid_size=size + 1, tile_size=128, distance_transition=128, grid_initial=grid_color)
 
     def press(event):
         if event.key == "up":
